c159553ece2bd2d77af725c194fb7222f2ffb1dc
S <xlaraslx.2005@gmail.com>
2026-06-20 18:00:16 +0330

    make main fucntion

--- RandomPass.py
+++ RandomPass.py
@@ -14,3 +14,24 @@ def generate_password():
 def save_password(username, password):
     with open("passwords.txt", "a", encoding="utf-8") as file:
         file.write(f"{username} : {password}\n")
+
+def main():
+    username = input("Enter username: ")
+
+    while True:
+        password = generate_password()
+
+        print("\nSuggested password:")
+        print(password)
+
+        choice = input(
+            "\nClick 's' for saving password, and click anything else to remake password: "
+        )
+
+        if choice.lower() == "s":
+            save_password(username, password)
+            print("Password saved.")
+            break
+
+
+main()
